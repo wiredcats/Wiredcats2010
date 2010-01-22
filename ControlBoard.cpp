9ced894ae2091a64b781404d39f92d9025c60a79
The Westminster Wiredcats (FRC 2415) <wiredcats2415@gmail.com>
2010-01-21 20:34:03 -0500

    Started work on mapping control board

--- ControlBoard.cpp
+++ ControlBoard.cpp
@@ -3,6 +3,7 @@
 ControlBoard::ControlBoard() {
 	leftStick = new Joystick(RIGHT_JOY_PORT);
 	rightStick = new Joystick(LEFT_JOY_PORT);
+	ds = DriverStation::GetInstance();
 }
 
 Joystick* ControlBoard::GetLeftJoy() {
@@ -11,4 +12,20 @@ Joystick* ControlBoard::GetLeftJoy() {
 
 Joystick* ControlBoard::GetRightJoy() {
 	return rightStick;
+}
+
+bool ControlBoard::TiltUpperLimitLit() {
+	return ds->GetDigitalOut(TILT_UP_LIMIT_DIO);
+}
+
+bool ControlBoard::TiltLowerLimitLit() {
+	return ds->GetDigitalOut(TILT_LOW_LIMIT_DIO);
+}
+
+bool ControlBoard::ExtendUpperLimitLit() {
+	return ds->GetDigitalOut(EXTEND_UP_LIMIT_DIO);
+}
+
+bool ControlBoard::ExtendLowerLimitLit() {
+	return ds->GetDigitalOut(EXTEND_LOW_LIMIT_DIO);
 }
--- ControlBoard.h
+++ ControlBoard.h
@@ -7,14 +7,25 @@
 const UINT32 RIGHT_JOY_PORT = 1;
 const UINT32 LEFT_JOY_PORT = 2;
 
+const UINT32 TILT_UP_LIMIT_DIO = 1;
+const UINT32 TILT_LOW_LIMIT_DIO = 2;
+const UINT32 EXTEND_UP_LIMIT_DIO = 3;
+const UINT32 EXTEND_LOW_LIMIT_DIO = 4;
+
 class ControlBoard {
 public:
 	Joystick *leftStick, *rightStick;
+	DriverStation *ds;
 	
 	ControlBoard();
 	
 	Joystick* GetLeftJoy();
 	Joystick* GetRightJoy();
+	
+	bool TiltUpperLimitLit();
+	bool TiltLowerLimitLit();
+	bool ExtendUpperLimitLit();
+	bool ExtendLowerLimitLit();
 };
 
 #endif
