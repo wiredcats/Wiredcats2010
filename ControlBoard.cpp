81f54e90aa65cf1a64cb8f3e90f1560f96dccb40
Lilly Chin <hm29168@gmail.com>
2010-02-07 17:44:36 -0500

    Control Board is mapped

--- ControlBoard.cpp
+++ ControlBoard.cpp
@@ -3,6 +3,7 @@
 ControlBoard::ControlBoard() {
 	leftStick = new Joystick(RIGHT_JOY_PORT);
 	rightStick = new Joystick(LEFT_JOY_PORT);
+	fakeButtonsStick = new Joystick(ALL_BUTTONS_PORT);
 }
 
 Joystick* ControlBoard::GetLeftJoy() {
@@ -11,4 +12,40 @@ Joystick* ControlBoard::GetLeftJoy() {
 
 Joystick* ControlBoard::GetRightJoy() {
 	return rightStick;
+}
+
+bool ControlBoard::GetButton1() {
+	return fakeButtonsStick->GetRawButton(5);
+}
+
+bool ControlBoard::GetButton2() {
+	return fakeButtonsStick->GetRawButton(3);
+}
+
+bool ControlBoard::GetButton3() {
+	return fakeButtonsStick->GetRawButton(4);
+}
+
+bool ControlBoard::GetLever1Up() {
+	return fakeButtonsStick->GetRawButton(10);
+}
+
+bool ControlBoard::GetLever1Down() {
+	return fakeButtonsStick->GetRawButton(11);
+}
+
+bool ControlBoard::GetLever2Up() {
+	return fakeButtonsStick->GetRawButton(9);
+}
+
+bool ControlBoard::GetLever2Down() {
+	return fakeButtonsStick->GetRawButton(8);
+}
+
+bool ControlBoard::GetLever3Up() {
+	return fakeButtonsStick->GetRawButton(7);
+}
+
+bool ControlBoard::GetLever3Down() {
+	return fakeButtonsStick->GetRawButton(6);
 }
--- ControlBoard.h
+++ ControlBoard.h
@@ -6,15 +6,26 @@
 // Constants
 const UINT32 RIGHT_JOY_PORT = 1;
 const UINT32 LEFT_JOY_PORT = 2;
+const UINT32 ALL_BUTTONS_PORT = 3;
 
 class ControlBoard {
 public:
-	Joystick *leftStick, *rightStick;
+	Joystick *leftStick, *rightStick, *fakeButtonsStick;
 	
 	ControlBoard();
 	
 	Joystick* GetLeftJoy();
 	Joystick* GetRightJoy();
+	Joystick* GetServeBallButton();
+	bool GetButton1();
+	bool GetButton2();
+	bool GetButton3();
+	bool GetLever1Up();
+	bool GetLever1Down();
+	bool GetLever2Up();
+	bool GetLever2Down();
+	bool GetLever3Up();
+	bool GetLever3Down();
 };
 
 #endif
