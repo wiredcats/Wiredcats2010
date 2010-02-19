82f44c34f2c7fef2c698d3119cc4bd5aeda6e190
David Allen <davialln@gmail.com>
2010-02-19 17:01:08 -0500

    Lilly on David's - Sorta kinda Kicker

--- Kicker.h
+++ Kicker.h
@@ -4,16 +4,34 @@
 #include "WPILib.h"
 #include "CAN/CANJaguar.h"
 
-const UINT32 SERVO_PORT = 4;
-const UINT32 WINCH_PORT = 5;
+const UINT32 ENCODER_CHANNEL_1 = 4;
+const UINT32 ENCODER_CHANNEL_2 = 5;
+
+const int SPIKE_NUM = 3;
+
+//Distances Encoder need to go from 0.
+const int LOW_GEAR_DIST = 100;
+const int MID_GEAR_DIST = 200;
+const int HIGH_GEAR_DIST = 300;
+
+typedef enum {kOff, kOn, kForward, kReverse} Value;
 
 class Kicker {
 public:
-	Servo* servo;
+	Encoder* encoder;
+	Relay* spike;
+	Compressor* compressor;
 	
 	Kicker();
-	void disengageServo();
-	void engageServo();
+	void ResetEncoder();
+	
+	void GoToLowGear();
+	void GoToMidGear();
+	void GoToHighGear();
+	
+	void KickBall();
+	
+	
 }; 
 
 #endif
